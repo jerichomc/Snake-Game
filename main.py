7b71f57d5cc121ba78a4508d5463bdf71a668e1d
jerichomc <149268694+jerichomc@users.noreply.github.com>
2024-02-05 18:23:55 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+
+screen = Screen()
+screen.setup(width=600, height=600) #sizes the screen
+screen.bgcolor("black") #sets background color
+screen.title("Snake") #titles the game/program
+screen.tracer(0) #Turn turtle animation on/off and set delay for update drawings
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")  #when one of these buttons is detected, the corresponding method is called
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    #detect food collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase()
+
+    #detect collision with wall
+    if snake.head.xcor() > 285 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -285:
+        scoreboard.reset()
+        snake.reset()
+
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+
+
+
+
+screen.exitonclick()
